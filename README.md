bae861855af1ddf60d8b01fba3c179ba9b4a2eea
Руслан Трофимов <edmondkoko777@gmail.com>
2022-12-15 00:20:48 +0300

    Финальный проект спринта: Бот-ассистент

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# homework_bot
-python telegram bot
+homework_bot
+«Telegram-бот для проверки домашки»
+Телеграм бот, который проверяет статус домашней работы и присылает результат в чат
+
+Технологии
+flake8==3.9.2
+flake8-docstrings==1.6.0
+pytest==6.2.5
+python-dotenv==0.19.0
+python-telegram-bot==13.7
+requests==2.26.0
+telegram~=0.0.1
+simplejson~=3.18.0
+
+Установите и активируйте виртуальное окружение
+Установите зависимости из файла requirements.txt
+pip install -r requirements.txt
+Для запуска используйте
+python homework.py
+Авторы
+Трофимов Руслан
--- homework.py
+++ homework.py
@@ -3,7 +3,7 @@ import os
 import sys
 import time
 from http import HTTPStatus
-
+import simplejson
 import requests
 import telegram
 from dotenv import load_dotenv
@@ -24,20 +24,19 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s, %(levelname)s, %(message)s',
-    handlers=[
-        logging.FileHandler('output.log'),
-        logging.StreamHandler(sys.stdout)
-    ]
-)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setStream(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def check_tokens():
     """Проверка токена."""
     if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
-        logging.critical('Отсутствует одна из переменных окружения')
+        logger.critical('Отсутствует одна из переменных окружения')
         return False
     return True
 
@@ -46,10 +45,10 @@ def send_message(bot, message):
     """Отправка сообщения в Telegram."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logging.debug('Отправка сообщения')
-    except Exception as error:
-        logging.error(f'Не удалось отправить сообщение: {error}')
-        raise Exception(error)
+        logger.debug('Отправка сообщения')
+    except telegram.error.TelegramError as error:
+        logger.error(f'Не удалось отправить сообщение: {error}')
+        raise Exception('Не удалось отправить сообщение')
 
 
 def get_api_answer(timestamp):
@@ -61,13 +60,14 @@ def get_api_answer(timestamp):
             params={'from_date': timestamp}
         )
     except requests.RequestException as error:
-        logging.error(f'Ошибка при запросе к API: {error}')
+        logger.error(f'Ошибка при запросе к API: {error}')
     if response.status_code != HTTPStatus.OK:
-        raise ConnectionError
-    response = response.json()
-    if not isinstance(response, dict):
-        raise TypeError
-    return response
+        raise ConnectionError('Ошибка при запросе к API')
+    try:
+        return response.json()
+    except simplejson.errors.JSONDecodeError:
+        logger.error('Ошибка ответа сервера')
+        send_message('Ошибка ответа сервера')
 
 
 def check_response(response):
@@ -75,10 +75,10 @@ def check_response(response):
     try:
         homeworks = response['homeworks']
     except KeyError as error:
-        logging.error(f'Ошибка доступа по ключу: {error}')
+        logger.error(f'Ошибка доступа по ключу: {error}')
     if not isinstance(homeworks, list):
-        logging.error('Не верный формат ответа')
-        raise TypeError('Не верный формат ответа')
+        logger.error('Неверный формат ответа')
+        raise TypeError('Неверный формат ответа')
     return homeworks
 
 
@@ -86,26 +86,26 @@ def parse_status(homework):
     """Получение статуса работы."""
     status = homework.get('status')
     if status is None:
-        raise TypeError
+        raise TypeError('Ошибка статуса')
     if status not in HOMEWORK_VERDICTS:
-        raise TypeError
+        raise TypeError('Неверный статус')
     homework_name = homework.get('homework_name')
     if homework_name is None:
-        raise TypeError
+        raise TypeError('Ошибка имени статуса')
     verdict = HOMEWORK_VERDICTS.get(status)
     if verdict is None:
-        logging.error(f'Неожиданный статус домашней работы - {status}')
+        logger.error(f'Неожиданный статус домашней работы - {status}')
         return None
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-    logging.info('Старт')
+    logger.info('Старт')
     if not check_tokens():
         sys.exit()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = 0
+    timestamp = int(time.time()) - RETRY_PERIOD
     prev_message: str = ''
     while True:
         try:
@@ -115,13 +115,13 @@ def main():
                 message = parse_status(homeworks[0])
             else:
                 message = prev_message
-                logging.debug('Нет изменений')
+                logger.debug('Нет изменений')
             if message != prev_message:
                 send_message(bot, message)
                 prev_message = message
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            logging.error(message)
+            logger.error(message)
             if message != prev_message:
                 send_message(bot, message)
                 prev_message = message
